e0c236a2dbe3243de36ae447f7e21614d926981e
Dibakar1234 <94843361+Dibakar1234@users.noreply.github.com>
2021-11-24 11:12:08 +0530

    Update Loginquery.php

--- Loginquery.php
+++ Loginquery.php
@@ -3,38 +3,48 @@
 $email = $_POST["email"];
 $password = $_POST["pass"];
 if(!($email=="admin@mail.com"))		// checking if user is admin or not,
-{									// normal user part
-$sql = "select * from reg where email='".$email."' and password='".$password."';";
-$select = mysqli_query($con,$sql);
-$row=mysqli_fetch_array($select);
-if($row)
 {
-	
-	$_SESSION["user"] = $email;
-	@header("location:WelcomeUser.php");
+										// normal user part					                                    
+$sql = "select * from reg where email='".$email."';";
+$select = mysqli_query($con,$sql);
+
+if(mysqli_num_rows($select) == 1)
+{
+	while($row=mysqli_fetch_array($select))
+	{
+		if(password_verify($password, $row["password"]))
+		{
+			$_SESSION["user"] = $email;
+			@header("location:WelcomeUser.php");
+		}
+		else
+			echo "Password and Email not matched";
+	}
 }
 else
 	echo "Password and Email not matched";
+	
 }
 else
 {										// Admin section
-$sql = "select * from admin where email='".$email."' and password='".$password."';";
+$sql = "select * from admin where email='".$email."';";
 $select = mysqli_query($con,$sql);
-$row=mysqli_fetch_array($select);
-if($row)
+
+if(mysqli_num_rows($select) == 1)
 {
-	
-	$_SESSION["admin"] = $email;
-	@header("location:WelcomeAdmin.php");
+	while($row = mysqli_fetch_array($select))
+	{
+		if(password_verify($password, $row["password"]))
+		{
+			$_SESSION["admin"] = $email;
+			@header("location:WelcomeAdmin.php");
+		}
+		else
+			echo "Password and Email not matched";
+	}
 }
 else
 	echo "Password and Email not matched";
 }
 
-
-
-
-
 ?>
-
-
