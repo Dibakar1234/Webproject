212297141e99a99bae88dd1fc78c76f8d2372cde
Dibakar1234 <94843361+Dibakar1234@users.noreply.github.com>
2021-11-24 16:43:59 +0530

    Update reg.php

--- reg.php
+++ reg.php
@@ -10,38 +10,32 @@
 	<form action="regquery.php" method="post" onsubmit="return validate();" >
 		<center>
 			<div ><h1>Registration </h1><br>
-				Email:<input type="text" name="email" id="email" placeholder="Enter your email"><br>
-				Password:<input type="password" name="pass" id="pass" placeholder="Enter your password"> <br>
-				Confirm Password:<input type="password" name="cpass" id="cpass" placeholder="Enter your password again"><br>
+				Email:<input type="text" name="email" id="email" placeholder="Enter your email"><br><br>
+				Password:<input type="password" name="pass" id="pass" placeholder="Enter your password"> <br><br>
+				Confirm Password:<input type="password" name="cpass" id="cpass" placeholder="Enter your password again"><br><br>
 				City: <select name="city" id="city">
 					<option value="-1" selected>--Select--</option>
 					<option value="Kolkata">Kolkata</option>
 					<option value="Mumbai">Mumbai</option>
 					<option value="Bangalore" >Bangalore</option>
-				</select><br>
+				</select><br><br>
 				Programming languages:
 				<input type="checkbox" name="lang[]" id="c" value="c"/>
 				<label>C</label>
 				<input type="checkbox" name="lang[]" id="java" value="java"/>
 				<label>Java</label>
 				<input type="checkbox" name="lang[]" id="php" value="php" />
-				<label>PHP</label><br>
+				<label>PHP</label><br><br>
 				Gender: 
 				<input type="radio" name="gen" id="gen" value="male"/>Male
-				<input type="radio" name="gen" id="gen" value="female"/>Female<br>
-				Select image:<input type="file" name="img" id="img" accept="image/*"><br>
-				<input type="submit" name="submit" value="Submit"><br>
-				<input type="reset" name="res">
-
+				<input type="radio" name="gen" id="gen" value="female"/>Female<br><br>
+				Select image:<input type="file" name="img" id="img" accept="image/*"><br><br>
+				<input type="submit" name="submit" value="Submit">&nbsp;&nbsp;&nbsp;&nbsp;
+				<input type="reset" name="res"><br><br>				
 			</div>
-
 		</center>
-		
-
 	</form>
-
-		
-
+<center>Already have an account?&nbsp;&nbsp;<a href="login.php"><button>Login</button></a></center>
 </body>
 </html>
 <script type="text/javascript">
