130e0ff99e03775035e57def384642b795bb22bc
Dibakar1234 <94843361+Dibakar1234@users.noreply.github.com>
2021-11-24 16:47:25 +0530

    Add files via upload

---
+++ sendmail.php
@@ -0,0 +1,23 @@
+<?php include("conn.php");
+
+$to_email = $_POST["email2"];
+$sql = "select * from reg where email='".$to_email."';";
+$result = mysqli_query($con, $sql);
+$num = mysqli_num_rows($result);
+if($num == 1)
+{
+	while($row = mysqli_fetch_array($result))
+	{
+		$pass = $row["password"];
+		$subject = "Simple Email Test for recover password using PHP";
+		$body = "Hi, your password is: ".$pass;
+		$headers = "From: mailtestphp9@gmail.com"; 
+		if (mail($to_email, $subject, $body, $headers)) 
+    		echo "Your password successfully sent to Email id: ".$to_email; 
+		else 
+    		echo "Email sending failed...";		
+	}
+
+}
+else
+	echo "Email id not exists";
